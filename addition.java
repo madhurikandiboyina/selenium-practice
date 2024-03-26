d0b7b5809ef5095e250f5d02428760c559983c44
madhurikandiboyina <165065089+madhurikandiboyina@users.noreply.github.com>
2024-03-26 22:30:59 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,13 @@
+package new_proj;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a=2;
+      int b=4;
+      int v=a+b;
+      System.out.println(v);
+	}
+
+}
---
+++ ecelhandling.java
@@ -0,0 +1,10 @@
+package new_proj;
+
+public class ecelhandling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
